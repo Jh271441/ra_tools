d4e647f83c568af069ae21ccd8be614e8c2029c8
jasperchen <jasperchen@didiglobal.com>
2026-07-31 18:19:03 +0800

    refine(dashboard): simplify batch draft fields

--- ra_triage_dashboard/static/app.js
+++ ra_triage_dashboard/static/app.js
@@ -1368,7 +1368,7 @@ function defaultPredictionBatchName() {
   const pad = (value) => String(value).padStart(2, "0");
   const date = `${now.getFullYear()}${pad(now.getMonth() + 1)}${pad(now.getDate())}`;
   const time = `${pad(now.getHours())}${pad(now.getMinutes())}${pad(now.getSeconds())}`;
-  return `${predictionActorSlug()}_i_${date}_${time}`;
+  return `${predictionActorSlug()}_${date}_${time}`;
 }
 
 function ensurePredictionBatchName() {
