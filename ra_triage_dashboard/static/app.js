4e13b1c414614c11b4a8099431360309dc5fc0d0
Junhao <609752056@qq.com>
2026-08-17 21:30:26 +0800

    align Trail update typography

--- ra_triage_dashboard/static/app.js
+++ ra_triage_dashboard/static/app.js
@@ -9,7 +9,7 @@
  * filenames are domain names without numeric prefixes.
  */
 (() => {
-  const CACHE_VERSION = "manual-triage-174";
+  const CACHE_VERSION = "manual-triage-175";
   const MODULES = [
     "core-base.js",
     "i18n-messages.js",
