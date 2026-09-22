12ef95ed765f314db11eff84968469eab17bb927
Junhao <609752056@qq.com>
2026-09-22 19:02:44 +0800

    Update migration count contract

--- ra_triage_dashboard/tests/test_postgres_integration.py
+++ ra_triage_dashboard/tests/test_postgres_integration.py
@@ -239,7 +239,7 @@ class PostgresDatabaseIntegrationTest(unittest.TestCase):
                 migration_count = conn.execute(
                     "SELECT COUNT(*) AS count FROM dashboard_schema_migrations"
                 ).fetchone()["count"]
-            self.assertEqual(int(migration_count), 50)
+            self.assertEqual(int(migration_count), 51)
         finally:
             database.close()
             other.close()
